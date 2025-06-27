2895adf09d15164a12634186e0174f9a59e72e75
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-27 19:58:06 -0300

    feat: initial React app scaffold

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function App() {
+  return (
+    <div className='container'>
+      <h1>Rehearsal Scheduler</h1>
+      <p>Schedule band rehearsals, send reminders & track attendance.</p>
+      {/* Auth, booking calendar and dashboard components go here */}
+    </div>
+  );
+}
+export default App;
